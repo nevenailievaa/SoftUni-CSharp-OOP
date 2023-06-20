485f827592be6c108407c6da195a463adc5ae493
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-20 16:59:44 +0300

    Add files via upload

---
+++ 01. Inheritance/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿namespace StackОfStrings
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+
+    }
+}

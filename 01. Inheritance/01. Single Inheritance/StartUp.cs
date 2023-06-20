85d487beff7223e218471ac8351b2d7c6eb283fc
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-20 15:20:44 +0300

    Add files via upload

---
+++ 01. Inheritance/01. Single Inheritance/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Eat();
+        }
+    }
+}

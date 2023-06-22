eaf636ba1aa05d43a886357dc16b110caf65e442
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-22 17:37:23 +0300

    Add files via upload

---
+++ 01. Inheritance/02. Inheritance - Exercise/01. Person/Child.cs
@@ -0,0 +1,9 @@
+﻿namespace Person;
+
+public class Child : Person
+{
+    //Constructor
+    public Child(string name, int age) : base(name, age){}
+
+
+}
---
+++ 01. Inheritance/02. Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Person;
+
+public class Person
+{
+    //Constructor
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    //Properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    //Methods
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.Append($"Name: {Name}, Age: {Age}");
+
+        return stringBuilder.ToString();
+    }
+}

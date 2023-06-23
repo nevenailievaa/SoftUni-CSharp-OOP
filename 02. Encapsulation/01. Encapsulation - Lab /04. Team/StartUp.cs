469439ff69aa7205902a3d13f1fc4401bce3161c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-23 19:52:41 +0300

    Add files via upload

---
+++ 02. Encapsulation/01. Encapsulation - Lab /04. Team/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //INPUT
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+            Team team = new Team("SoftUni");
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] commandInfo = Console.ReadLine().Split();
+                Person person = new Person(commandInfo[0], commandInfo[1], int.Parse(commandInfo[2]), decimal.Parse(commandInfo[3]));
+
+                people.Add(person);
+                team.AddPlayer(person);
+            }
+
+            //OUTPUT
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 02. Encapsulation/01. Encapsulation - Lab /04. Team/Team.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo;
+
+public class Team
+{
+    //Fields
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    //Constructor
+    public Team(string name)
+    {
+        this.name = name;
+        firstTeam = new List<Person>();
+        reserveTeam = new List<Person>();
+    }
+
+    //Properties
+    public string Name { get { return name; } private set { name = value; } }
+    public IReadOnlyCollection<Person> FirstTeam => firstTeam.AsReadOnly();
+    public IReadOnlyCollection<Person> ReserveTeam => reserveTeam.AsReadOnly();
+
+    //Methods
+    public void AddPlayer(Person person)
+    {
+        if (person.Age < 40)
+        {
+            firstTeam.Add(person);
+        }
+        else
+        {
+            reserveTeam.Add(person);
+        }
+    }
+}

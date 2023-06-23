fda3a3f526c2c0313da9052d084fd390cb2c03d7
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-23 17:40:00 +0300

    Add files via upload

---
+++ 02. Encapsulation/01. Encapsulation - Lab /02. Salary/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace PersonsInfo;
+
+public class Person
+{
+    //Constructor
+    public Person(string firstName, string lastName, int age, decimal salary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Salary = salary;
+    }
+
+    //Properties
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public int Age { get; private set; }
+    public decimal Salary { get; private set; }
+
+    //Methods
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+    }
+
+    public decimal IncreaseSalary(decimal percentage)
+    {
+        if (Age < 30)
+        {
+            percentage /= 2;
+        }
+
+        Salary += (Salary * (percentage / 100));
+        return Salary;
+    }
+}

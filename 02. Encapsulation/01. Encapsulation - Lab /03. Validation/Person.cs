372434eb671d84f5c317845a90b52c92c7b32c94
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-23 18:38:53 +0300

    Add files via upload

---
+++ 02. Encapsulation/01. Encapsulation - Lab /03. Validation/Person.cs
@@ -0,0 +1,110 @@
+﻿namespace PersonsInfo;
+
+public class Person
+{
+    //Fields
+    private string firstName;
+    private string lastName;
+    private int age;
+    private int salary;
+
+    //Constructor
+    public Person(string firstName, string lastName, int age, decimal salary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Salary = salary;
+    }
+
+    //Properties
+    public string FirstName
+    {
+        get
+        {
+            return firstName;
+        }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
+            else
+            {
+                firstName = value;
+            }
+        }
+    }
+
+    public string LastName
+    {
+        get
+        {
+            return lastName;
+        }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+            else
+            {
+                lastName = value;
+            }
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+            else
+            {
+                age = value;
+            }
+        }
+    }
+
+    public decimal Salary
+    {
+        get
+        {
+            return salary;
+        }
+        private set
+        {
+            if (value < 650)
+            {
+                throw new ArgumentException("Salary cannot be less than 650 leva!");
+            }
+        }
+    }
+
+
+    //Methods
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+    }
+
+    public decimal IncreaseSalary(decimal percentage)
+    {
+        if (Age < 30)
+        {
+            percentage /= 2;
+        }
+
+        Salary += (Salary * (percentage / 100));
+        return Salary;
+    }
+}
---
+++ 02. Encapsulation/01. Encapsulation - Lab /03. Validation/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //INPUT
+            int peopleCount = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+
+                people.Add(person);
+            }
+
+            decimal parcentage = decimal.Parse(Console.ReadLine());
+            people.ForEach(p => p.IncreaseSalary(parcentage));
+
+            //OUTPUT
+            people.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}

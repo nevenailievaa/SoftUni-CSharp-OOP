9c58147fd6f04fe44ccd9c0c57df00ce70a75728
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-23 17:40:15 +0300

    Add files via upload

---
+++ 02. Encapsulation/01. Encapsulation - Lab /01. Persons/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace PersonsInfo;
+
+public class Person
+{
+    //Constructor
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    //Properties
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public int Age { get; private set; }
+
+    //Methods
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} is {Age} years old.";
+    }
+}
---
+++ 02. Encapsulation/01. Encapsulation - Lab /01. Persons/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //INPUT
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            //ACTION
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]));
+                people.Add(person);
+            }
+
+            //OUTPUT
+            foreach (Person person in people.OrderBy(p => p.FirstName))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}

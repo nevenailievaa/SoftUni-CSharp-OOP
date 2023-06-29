061d3e705a18c3e1ac65cf696798a392c91a88c7
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-29 12:59:57 +0300

    Add files via upload

---
+++ 02. Encapsulation/02. Encapsulation - Exercise/03. Shopping Spree/Models/Person.cs
@@ -0,0 +1,69 @@
+﻿namespace ShoppingSpree.Models;
+
+public class Person
+{
+    //Fields
+    private string name;
+    private decimal money;
+    private List<Product> products;
+
+    //Constructor
+    public Person(string name, decimal money)
+    {
+        Name = name;
+        Money = money;
+        products = new List<Product>();
+    }
+
+    //Properties
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+            name = value;
+        }
+    }
+    public decimal Money
+    {
+        get => money;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Money cannot be negative");
+            }
+            money = value;
+        }
+    }
+
+    //Methods
+    public string BuyProduct(Product product)
+    {
+        if (Money < product.Cost)
+        {
+            return $"{Name} can't afford {product.Name}";
+        }
+
+        products.Add(product);
+        Money -= product.Cost;
+
+        return $"{Name} bought {product.Name}";
+    }
+
+    public override string ToString()
+    {
+        if(products.Any())
+        {
+            return $"{Name} - {String.Join(", ", products.Select(p => p.Name))}";
+        }
+        else
+        {
+            return $"{Name} - Nothing bought";
+        }
+    }
+}

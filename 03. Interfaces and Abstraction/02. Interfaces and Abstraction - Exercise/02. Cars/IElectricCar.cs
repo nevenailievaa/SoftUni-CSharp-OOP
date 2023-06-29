c116d09f498843c7c57714d0b25a899be7596f6b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-29 23:30:37 +0300

    Add files via upload

---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/02. Cars/IElectricCar.cs
@@ -0,0 +1,6 @@
+﻿namespace Cars;
+
+public interface IElectricCar : ICar
+{
+    public int Battery { get; set; }
+}
---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/02. Cars/Icar.cs
@@ -0,0 +1,11 @@
+﻿namespace Cars;
+
+public interface ICar
+{
+    //Properties
+    string Model { get; set; }
+    string Color { get; set; }
+
+    public string Start();
+    public string Stop();
+}

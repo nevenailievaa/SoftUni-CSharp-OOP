8ae349f0d2390de6fcde9b67d4eefdfb5e834193
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-29 12:57:56 +0300

    Add files via upload

---
+++ 02. Encapsulation/02. Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,70 @@
+﻿namespace ClassBoxData;
+
+public class Box
+{
+    //Fields
+    private const string PropertyValueExceptionMessage = "{0} cannot be zero or negative.";
+    private double length;
+    private double width;
+    private double height;
+
+    //Constructor
+    public Box(double length, double width, double height)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+    }
+
+    //Properties
+    public double Length
+    {
+        get => length;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(String.Format(PropertyValueExceptionMessage, nameof(Length)));
+            }
+            else
+            {
+                length = value;
+            }
+        }
+    }
+    public double Width
+    {
+        get => width;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(String.Format(PropertyValueExceptionMessage, nameof(Width)));
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    public double Height
+    {
+        get => height;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(String.Format(PropertyValueExceptionMessage, nameof(Height)));
+            }
+            else
+            {
+                height  = value;
+            }
+        }
+    }
+
+    //Methods
+    public double SurfaceArea() => (2 * Length * Width) + (2 * Length * Height) + (2 * Width * height);
+    public double LeteralSurfaceArea() => (2 * Length * Height) + (2 * Width * Height);
+    public double Volume() => length * width * height;
+}
---
+++ 02. Encapsulation/02. Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,19 @@
+﻿//INPUT
+using ClassBoxData;
+
+double length = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+try
+{
+    Box box = new Box(length, width, height);
+
+    Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+    Console.WriteLine($"Lateral Surface Area - {box.LeteralSurfaceArea():f2}");
+    Console.WriteLine($"Volume - {box.Volume():f2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}

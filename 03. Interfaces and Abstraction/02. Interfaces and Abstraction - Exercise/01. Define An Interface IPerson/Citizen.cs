64bc36857e7f15cbb9e66170dd354d48eaf706a9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-30 15:30:16 +0300

    Add files via upload

---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/01. Define An Interface IPerson/Citizen.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonInfo;
+
+public class Citizen : IPerson
+{
+    //Fields
+    private string name;
+    private int age;
+
+    //Constructor
+    public Citizen(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    //Properties
+    public string Name { get => name; set => name = value; }
+    public int Age { get => age; set => age = value; }
+}

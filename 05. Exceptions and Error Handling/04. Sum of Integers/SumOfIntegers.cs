c6aa180e0a17de5675eefbe41ea8938cd56f22a2
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-07-05 21:47:28 +0300

    Add files via upload

---
+++ 05. Exceptions and Error Handling/04. Sum of Integers/SumOfIntegers.cs
@@ -0,0 +1,28 @@
+﻿//Input
+string[] elements = Console.ReadLine().Split(" ");
+
+//ACTION
+int sum = 0;
+
+foreach (string element in elements)
+{
+    try
+    {
+        int currentNum = int.Parse(element);
+        sum += currentNum;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{element}' is in wrong format!");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{element}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+    }
+}
+
+Console.WriteLine($"The total sum of all integers is: {sum}");

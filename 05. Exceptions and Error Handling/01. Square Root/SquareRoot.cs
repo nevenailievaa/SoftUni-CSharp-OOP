1c3b92638ec00408905252e531c1cbf3014c07bc
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-07-05 21:46:46 +0300

    Add files via upload

---
+++ 05. Exceptions and Error Handling/01. Square Root/SquareRoot.cs
@@ -0,0 +1,21 @@
+﻿//Action
+try
+{
+    //Input
+    int inputNumber = int.Parse(Console.ReadLine());
+
+	if (inputNumber < 0)
+	{
+		throw new ArgumentOutOfRangeException(nameof(inputNumber), "Invalid number.");
+	}
+
+    Console.WriteLine(Math.Sqrt(inputNumber));
+}
+catch (ArgumentOutOfRangeException ex)
+{
+	Console.WriteLine("Invalid number.");
+}
+finally
+{
+	Console.WriteLine("Goodbye.");
+}

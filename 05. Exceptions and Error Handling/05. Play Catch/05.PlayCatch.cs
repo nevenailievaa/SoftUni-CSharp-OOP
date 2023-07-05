1ff194a8443f6b2cb36b24d1be86761fea0fcbf6
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-07-05 22:16:46 +0300

    Add files via upload

---
+++ 05. Exceptions and Error Handling/05. Play Catch/05.PlayCatch.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+//Input
+int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int exceptionsCaught = 0;
+
+//Action
+while (exceptionsCaught < 3)
+{
+    string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string commandType = command[0];
+
+	try
+	{
+        //Replace Command
+		if (commandType == "Replace")
+		{
+            int index = int.Parse(command[1]);
+            int element = int.Parse(command[2]);
+
+            numbers[index] = element;
+        }
+
+        //Print Command
+        else if (commandType == "Print")
+        {
+            int startIndex = int.Parse(command[1]);
+            int endIndex = int.Parse(command[2]);
+
+            if (startIndex < 0 || startIndex >= numbers.Length || endIndex < 0 || endIndex >= numbers.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (i < endIndex)
+                {
+                    Console.Write($"{numbers[i]}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers[i]}");
+                }
+            }
+        }
+
+        //Show Command
+        else if (commandType == "Show")
+        {
+            int index = int.Parse(command[1]);
+            Console.WriteLine(numbers[index]);
+        }
+    }
+	catch (IndexOutOfRangeException)
+	{
+		exceptionsCaught++;
+        Console.WriteLine("The index does not exist!");
+    }
+	catch (FormatException)
+	{
+		exceptionsCaught++;
+		Console.WriteLine("The variable is not in the correct format!");
+	}
+}
+
+//Output
+Console.WriteLine(String.Join(", ", numbers));

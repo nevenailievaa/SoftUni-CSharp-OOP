ac2c8605c8303c417c244f1cc3964a89e69e5824
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-07-13 11:22:47 +0300

    Add files via upload

---
+++ 07. Reflection and Attributes/01. Reflection and Attributes - Lab/02. High Quality Mistakes/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace Stealer;
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+        Console.WriteLine(result);
+    }
+}

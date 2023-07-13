407e5b2df13aab0dca95e3af34c3c0e4c3441c78
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-07-13 10:52:41 +0300

    Add files via upload

---
+++ 07. Reflection and Attributes/01. Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer;
+
+public class Spy
+{
+    public string StealFieldInfo(string investigatedClass, params string[] fields)
+    {
+        Type classType = Type.GetType(investigatedClass);
+        FieldInfo[] classFields = classType.GetFields((BindingFlags)60);
+        StringBuilder sb = new StringBuilder();
+        Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+        foreach (FieldInfo field in classFields.Where(f => fields.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ 07. Reflection and Attributes/01. Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Stealer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+        Console.WriteLine(result);
+    }
+}

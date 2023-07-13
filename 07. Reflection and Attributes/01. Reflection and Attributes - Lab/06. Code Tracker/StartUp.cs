028a9f512f79e37d06dc1fc98dee9870e5d2e3d2
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-07-13 13:49:31 +0300

    Add files via upload

---
+++ 07. Reflection and Attributes/01. Reflection and Attributes - Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    internal class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+    class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
